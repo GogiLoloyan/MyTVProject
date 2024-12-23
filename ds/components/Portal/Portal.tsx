f73b2750a8054f406c6cbe2f7e44556aef70fe9e
Gogi Loloyan <gogiloloyan11@gmail.com>
2024-12-23 14:03:00 +0400

    fix: import

---
+++ ds/components/Portal/Portal.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+
+import { InternalTheme } from '../../types';
+import PortalConsumer from './PortalConsumer';
+import PortalHost, { PortalContext, PortalMethods } from './PortalHost';
+import ThemeProvider, { withInternalTheme } from '../../styles/ThemeProvider';
+
+export type Props = {
+  /**
+   * Content of the `Portal`.
+   */
+  children: React.ReactNode;
+  /**
+   * @optional
+   */
+  theme: InternalTheme;
+};
+
+/**
+ * Portal allows rendering a component at a different place in the parent tree.
+ * You can use it to render content which should appear above other elements, similar to `Modal`.
+ * It requires a [`Portal.Host`](PortalHost) component to be rendered somewhere in the parent tree.
+ * Note that if you're using the `Provider` component, this already includes a `Portal.Host`.
+ *
+ * ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { Portal, Text } from '@ds/components';
+ *
+ * const MyComponent = () => (
+ *   <Portal>
+ *     <Text>This is rendered at a different place</Text>
+ *   </Portal>
+ * );
+ *
+ * export default MyComponent;
+ * ```
+ */
+class Portal extends React.Component<Props> {
+  // @component ./PortalHost.tsx
+  static Host = PortalHost;
+
+  render() {
+    const { children, theme } = this.props;
+
+    return (
+      //   <SettingsConsumer>
+      //     {(settings) => (
+      <PortalContext.Consumer>
+        {(manager) => (
+          <PortalConsumer manager={manager as PortalMethods}>
+            {/* <SettingsProvider value={settings}> */}
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            {/* </SettingsProvider> */}
+          </PortalConsumer>
+        )}
+      </PortalContext.Consumer>
+      //     )}
+      //   </SettingsConsumer>
+    );
+  }
+}
+
+export default withInternalTheme(Portal);
